9551a9e8ecdac4654071cccd368de2ae1c74692f
Cahox <calvinhong123@gmail.com>
2022-04-19 02:54:34 +0200

    added rabbitmq

---
+++ src/main/java/com/clashbots/contract/RabbitmqReceiver.java
@@ -0,0 +1,22 @@
+package com.clashbots.contract;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.amqp.rabbit.annotation.RabbitListenerConfigurer;
+import org.springframework.amqp.rabbit.listener.RabbitListenerEndpointRegistrar;
+import org.springframework.stereotype.Component;
+
+@Component
+public class RabbitmqReceiver implements RabbitListenerConfigurer {
+    private static final Logger logger = LoggerFactory.getLogger(RabbitmqReceiver.class);
+
+    @Override
+    public void configureRabbitListeners(RabbitListenerEndpointRegistrar rabbitListenerEndpointRegistrar) {
+    }
+
+    @RabbitListener(queues = "${spring.rabbitmq.queue}")
+    public void receivedMessage(Long user) {
+        logger.info("User Details Received is.. " + user);
+    }
+}
